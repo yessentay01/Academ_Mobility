c7abfbacbba61a89bece6763be85b3688612b016
TomXemmings <102224667+TomXemmings@users.noreply.github.com>
2023-02-24 09:38:14 +0600

    Fix content

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>IITU ACADEMIC MOBILITY</title>
         @vite('resources/css/app.css')
     </head>
     <body class="bg-slate-100">
@@ -61,7 +61,7 @@
             <div class="container px-5 py-24 mx-auto">
                 <div class="text-center mb-20">
                     <h1 class="sm:text-3xl text-2xl font-medium text-center title-font text-gray-900 mb-4">WHY DO I NEED IT?</h1>
-                    <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus eum in maiores voluptate voluptatum! Ab assumenda consectetur delectus esse laboriosam minima pariatur, quis rem sequi, sint, tempora tempore? Ex, sit!</p>
+                    <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">Studying abroad, the student becomes part of a multinational student Association, which has a positive impact on building an international career.</p>
                 </div>
                 <div class="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
                     <div class="p-2 sm:w-1/2 w-full">
@@ -125,7 +125,7 @@
             <div class="container px-5 py-24 mx-auto">
                 <div class="flex flex-wrap w-full mb-20 flex-col items-center text-center">
                     <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">WHAT I WILL GAIN?</h1>
-                    <p class="lg:w-1/2 w-full leading-relaxed text-gray-500">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, iste.</p>
+                    <p class="lg:w-1/2 w-full leading-relaxed text-gray-500">Studying abroad offers several advantages.</p>
                 </div>
                 <div class="flex flex-wrap -m-4">
                     <div class="xl:w-1/3 md:w-1/2 p-4">
@@ -136,7 +136,7 @@
                                 </svg>
                             </div>
                             <h2 class="text-lg text-gray-900 font-medium title-font mb-2">Self-confidence</h2>
-                            <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waist co, subway tile poke farm.</p>
+                            <p class="leading-relaxed text-base">Although it is available to everyone to study abroad, it is not awarded to everyone, if you can pass the competition, consider that you are the chosen one.</p>
                         </div>
                     </div>
                     <div class="xl:w-1/3 md:w-1/2 p-4">
@@ -147,7 +147,7 @@
                                 </svg>
                             </div>
                             <h2 class="text-lg text-gray-900 font-medium title-font mb-2">Responsibility</h2>
-                            <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waist co, subway tile poke farm.</p>
+                            <p class="leading-relaxed text-base">You need to understand that abroad you represent your people and your country, you are responsible for the image of a person from your homeland.</p>
                         </div>
                     </div>
                     <div class="xl:w-1/3 md:w-1/2 p-4">
@@ -159,7 +159,7 @@
                                 </svg>
                             </div>
                             <h2 class="text-lg text-gray-900 font-medium title-font mb-2">Independence</h2>
-                            <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waist co, subway tile poke farm.</p>
+                            <p class="leading-relaxed text-base">You are moving not just to another city, but to another country, where you will need to study the environment yourself.</p>
                         </div>
                     </div>
                     <div class="xl:w-1/3 md:w-1/2 p-4">
@@ -170,7 +170,7 @@
                                 </svg>
                             </div>
                             <h2 class="text-lg text-gray-900 font-medium title-font mb-2">Competitiveness</h2>
-                            <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waist co, subway tile poke farm.</p>
+                            <p class="leading-relaxed text-base">A student studying abroad will have an advantage over his peers both in an international and in an ordinary company.</p>
                         </div>
                     </div>
                     <div class="xl:w-1/3 md:w-1/2 p-4">
@@ -181,7 +181,7 @@
                                 </svg>
                             </div>
                             <h2 class="text-lg text-gray-900 font-medium title-font mb-2">Language</h2>
-                            <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waist co, subway tile poke farm.</p>
+                            <p class="leading-relaxed text-base">You can go to courses at least all your life, but to really master a language you need to get into the environment of its native speakers.</p>
                         </div>
                     </div>
                     <div class="xl:w-1/3 md:w-1/2 p-4">
@@ -193,7 +193,7 @@
 
                             </div>
                             <h2 class="text-lg text-gray-900 font-medium title-font mb-2">Communication skills</h2>
-                            <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waist co, subway tile poke farm.</p>
+                            <p class="leading-relaxed text-base">You will learn how to build relationships with representatives of another culture.</p>
                         </div>
                     </div>
                 </div>
@@ -278,43 +278,43 @@
                 </div>
             </div>
         </section>
-        <section class="text-gray-600 body-font">
-            <div class="container px-5 py-24 mx-auto">
-                <h1 class="text-3xl font-medium title-font text-gray-900 mb-12 text-center">Reviews</h1>
-                <div class="flex flex-wrap -m-4">
-                    <div class="p-4 md:w-1/2 w-full">
-                        <div class="h-full bg-gray-100 p-8 rounded">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="block w-5 h-5 text-gray-400 mb-4" viewBox="0 0 975.036 975.036">
-                                <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
-                            </svg>
-                            <p class="leading-relaxed mb-6">Synth chartreuse iPhone lomo cray raw denim brunch everyday carry neutra before they sold out fixie 90's microdosing. Tacos pinterest fanny pack venmo, post-ironic heirloom try-hard pabst authentic iceland.</p>
-                            <a class="inline-flex items-center">
-                                <img alt="testimonial" src="https://dummyimage.com/106x106" class="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center">
-                                <span class="flex-grow flex flex-col pl-4">
-                                  <span class="title-font font-medium text-gray-900">Holden Caulfield</span>
-                                  <span class="text-gray-500 text-sm">UI DEVELOPER</span>
-                                </span>
-                            </a>
-                        </div>
-                    </div>
-                    <div class="p-4 md:w-1/2 w-full">
-                        <div class="h-full bg-gray-100 p-8 rounded">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="block w-5 h-5 text-gray-400 mb-4" viewBox="0 0 975.036 975.036">
-                                <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
-                            </svg>
-                            <p class="leading-relaxed mb-6">Synth chartreuse iPhone lomo cray raw denim brunch everyday carry neutra before they sold out fixie 90's microdosing. Tacos pinterest fanny pack venmo, post-ironic heirloom try-hard pabst authentic iceland.</p>
-                            <a class="inline-flex items-center">
-                                <img alt="testimonial" src="https://dummyimage.com/107x107" class="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center">
-                                <span class="flex-grow flex flex-col pl-4">
-              <span class="title-font font-medium text-gray-900">Alper Kamu</span>
-              <span class="text-gray-500 text-sm">DESIGNER</span>
-            </span>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
+{{--        <section class="text-gray-600 body-font">--}}
+{{--            <div class="container px-5 py-24 mx-auto">--}}
+{{--                <h1 class="text-3xl font-medium title-font text-gray-900 mb-12 text-center">Reviews</h1>--}}
+{{--                <div class="flex flex-wrap -m-4">--}}
+{{--                    <div class="p-4 md:w-1/2 w-full">--}}
+{{--                        <div class="h-full bg-gray-100 p-8 rounded">--}}
+{{--                            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="block w-5 h-5 text-gray-400 mb-4" viewBox="0 0 975.036 975.036">--}}
+{{--                                <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>--}}
+{{--                            </svg>--}}
+{{--                            <p class="leading-relaxed mb-6">Synth chartreuse iPhone lomo cray raw denim brunch everyday carry neutra before they sold out fixie 90's microdosing. Tacos pinterest fanny pack venmo, post-ironic heirloom try-hard pabst authentic iceland.</p>--}}
+{{--                            <a class="inline-flex items-center">--}}
+{{--                                <img alt="testimonial" src="https://dummyimage.com/106x106" class="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center">--}}
+{{--                                <span class="flex-grow flex flex-col pl-4">--}}
+{{--                                  <span class="title-font font-medium text-gray-900">Holden Caulfield</span>--}}
+{{--                                  <span class="text-gray-500 text-sm">UI DEVELOPER</span>--}}
+{{--                                </span>--}}
+{{--                            </a>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                    <div class="p-4 md:w-1/2 w-full">--}}
+{{--                        <div class="h-full bg-gray-100 p-8 rounded">--}}
+{{--                            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="block w-5 h-5 text-gray-400 mb-4" viewBox="0 0 975.036 975.036">--}}
+{{--                                <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>--}}
+{{--                            </svg>--}}
+{{--                            <p class="leading-relaxed mb-6">Synth chartreuse iPhone lomo cray raw denim brunch everyday carry neutra before they sold out fixie 90's microdosing. Tacos pinterest fanny pack venmo, post-ironic heirloom try-hard pabst authentic iceland.</p>--}}
+{{--                            <a class="inline-flex items-center">--}}
+{{--                                <img alt="testimonial" src="https://dummyimage.com/107x107" class="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center">--}}
+{{--                                <span class="flex-grow flex flex-col pl-4">--}}
+{{--              <span class="title-font font-medium text-gray-900">Alper Kamu</span>--}}
+{{--              <span class="text-gray-500 text-sm">DESIGNER</span>--}}
+{{--            </span>--}}
+{{--                            </a>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </section>--}}
 
         <section id="universities" class="text-gray-600 body-font bg-white">
             <div class="container px-5 py-24 mx-auto">
@@ -323,7 +323,7 @@
                         <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Universities</h1>
                         <div class="h-1 w-20 bg-indigo-500 rounded"></div>
                     </div>
-                    <p class="lg:w-1/2 w-full leading-relaxed text-gray-500">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusantium aliquam assumenda dignissimos dolores doloribus enim, id in maiores numquam perspiciatis placeat porro provident quam quas quos saepe sed soluta!</p>
+                    <p class="lg:w-1/2 w-full leading-relaxed text-gray-500">You can get acquainted with our partners around the world, you need to choose a university that has places in your specialty, but the main thing is that you like your choice.</p>
                 </div>
                 <div class="flex flex-wrap -m-4">
                     @foreach($universities as $university)
