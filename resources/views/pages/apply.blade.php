c8311b6802ca7790496f90c7e58c618da07e9d44
TomXemmings <102224667+TomXemmings@users.noreply.github.com>
2023-09-21 13:34:37 +0600

    Update apply.blade.php

--- resources/views/pages/apply.blade.php
+++ resources/views/pages/apply.blade.php
@@ -299,7 +299,7 @@
         </div>
 
         <div class="flex items-center justify-end px-4 py-3 bg-gray-50 text-right sm:px-6 shadow dark:bg-gray-700">
-            <p>Я подтверждаю что все мои данные верны. <input type="checkbox"></p>
+            <p>Я подтверждаю что все мои данные верны. <input type="checkbox" style="margin: 0px 30px 0px 30px"></p>
             <button type="submit" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray transition ease-in-out duration-150">Apply</button>
         </div>
     </form>
