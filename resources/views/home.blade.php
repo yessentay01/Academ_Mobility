907e4923b9f1c28e412de8b8f7dcdc1975188306
TomXemmings <102224667+TomXemmings@users.noreply.github.com>
2023-09-21 15:25:40 +0600

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -275,6 +275,7 @@
         </div>
 
         <form method="POST" action="{{route('home.deleter')}}">
+            @csrf
             <div class="flex items-center justify-end px-4 py-3 bg-gray-50 text-right sm:px-6 shadow dark:bg-gray-700">
                 <button type="submit" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray transition ease-in-out duration-150" style="background-color: darkred !important;">Delete</button>
             </div>
