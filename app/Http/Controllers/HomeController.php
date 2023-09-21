3e77246d69ddbbcc0960e907c58d16eb79fcacef
TomXemmings <102224667+TomXemmings@users.noreply.github.com>
2023-09-21 16:37:18 +0600

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -64,4 +64,94 @@ class HomeController extends Controller
         }
         return redirect()->route('home')->with('success', 'Apply changed successfully');
     }
+
+    public function transcriptChanger(Request $request){
+        $request->validate([
+            'transcript' => 'required | mimes:pdf,png,webp,jpg,jpeg | max:16384', // 16MB
+        ]);
+        $user = auth()->user()->toArray();
+
+        $transcript_name = $request->transcript->hashName();
+        $request->transcript->storeAs('media/applies/', $transcript_name, 'public');
+
+        $applies = Apply::where('email', '=', $user['email'])->get();
+        if (isset($applies)) {
+            if (count($applies) > 0) {
+                $applyUser = Apply::where('email', '=', $user['email'])->update(['transcript'=>$transcript_name]);
+            }
+        }
+        return redirect()->route('home')->with('success', 'Apply changed successfully');
+    }
+
+    public function motivationChanger(Request $request){
+        $request->validate([
+            'motivation' => 'required | mimes:pdf,png,webp,jpg,jpeg | max:16384', // 16MB
+        ]);
+        $user = auth()->user()->toArray();
+
+        $motivation_name = $request->motivation->hashName();
+        $request->motivation->storeAs('media/applies/', $motivation_name, 'public');
+
+        $applies = Apply::where('email', '=', $user['email'])->get();
+        if (isset($applies)) {
+            if (count($applies) > 0) {
+                $applyUser = Apply::where('email', '=', $user['email'])->update(['motivation'=>$motivation_name]);
+            }
+        }
+        return redirect()->route('home')->with('success', 'Apply changed successfully');
+    }
+
+    public function englishConfChanger(Request $request){
+        $request->validate([
+            'confirmation' => 'required | mimes:pdf,png,webp,jpg,jpeg | max:16384', // 16MB
+        ]);
+        $user = auth()->user()->toArray();
+
+        $confirmation_name = $request->confirmation->hashName();
+        $request->confirmation->storeAs('media/applies/', $confirmation_name, 'public');
+
+        $applies = Apply::where('email', '=', $user['email'])->get();
+        if (isset($applies)) {
+            if (count($applies) > 0) {
+                $applyUser = Apply::where('email', '=', $user['email'])->update(['confirmation'=>$confirmation_name]);
+            }
+        }
+        return redirect()->route('home')->with('success', 'Apply changed successfully');
+    }
+
+    public function letterChanger(Request $request){
+        $request->validate([
+            'letter' => 'required | mimes:pdf,png,webp,jpg,jpeg | max:16384', // 16MB
+        ]);
+        $user = auth()->user()->toArray();
+
+        $letter_name = $request->letter->hashName();
+        $request->letter->storeAs('media/applies/', $letter_name, 'public');
+
+        $applies = Apply::where('email', '=', $user['email'])->get();
+        if (isset($applies)) {
+            if (count($applies) > 0) {
+                $applyUser = Apply::where('email', '=', $user['email'])->update(['letter'=>$letter_name]);
+            }
+        }
+        return redirect()->route('home')->with('success', 'Apply changed successfully');
+    }
+
+    public function passportScanChanger(Request $request){
+        $request->validate([
+            'passport_scan' => 'required | mimes:pdf,png,webp,jpg,jpeg | max:16384', // 16MB
+        ]);
+        $user = auth()->user()->toArray();
+
+        $passport_scan_name = $request->passport_scan->hashName();
+        $request->passport_scan->storeAs('media/applies/', $passport_scan_name, 'public');
+
+        $applies = Apply::where('email', '=', $user['email'])->get();
+        if (isset($applies)) {
+            if (count($applies) > 0) {
+                $applyUser = Apply::where('email', '=', $user['email'])->update(['passport_scan'=>$passport_scan_name]);
+            }
+        }
+        return redirect()->route('home')->with('success', 'Apply changed successfully');
+    }
 }
