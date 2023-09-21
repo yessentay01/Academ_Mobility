6db04b36c57404bc0a0e6f9a388ad6f006e7f5ca
TomXemmings <102224667+TomXemmings@users.noreply.github.com>
2023-09-21 16:20:38 +0600

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -230,6 +230,7 @@
                             @error('application_to_rector')
                             <p class="text-sm text-red-500"> {{ $message }} </p>
                             @enderror
+                            <button type="submit" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray transition ease-in-out duration-150">Update</button>
                         </form>
                     </div>
                 </div>
