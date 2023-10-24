ce8cff8d3ce9e4821dab4c9674e7d65a6dd30cb2
TomXemmings <102224667+TomXemmings@users.noreply.github.com>
2023-10-24 13:44:49 +0600

    Update MainController.php

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -21,14 +21,14 @@ class MainController extends Controller
             ["Innopolis University (Russia)", "28.10.23"],
             ["Сaucasus University (Georgia)", "11.11.2023"],
             ["Karadeniz Technical university (Turkey)", "11.11.2023"],
-            ["Nigde Omer Halisdemir University (Turkey)", "17.11.2023 (Turkish language level A2)"],
             ["Adam Mickiewicz University (Poland)", "15.10.23"],
 //            ["Innopolis University (Russia)", "05.10.23"],
             ["ИТМО (Russia)", "05.10.23"],
             ["ВШЭ (Russia)", "05.10.23"],
             ["UNIVERSITY OF ŽILINA (Slovak)", "30.09.23"],
             ["University of Debrecen (Hungary)", "30.09.23"],
-            ["ADA University (Azerbaijan)", "05.10.23"]
+            ["ADA University (Azerbaijan)", "05.10.23"],
+            ["Nigde Omer Halisdemir University (Turkey)", "17.11.2023 (Turkish language level A2)"]
         ];
 
         return view('welcome', compact('universities', 'deadLines'));
